8f9e2121b4e3f5f6dae6424ce3c705e176ea417d
jisli <jisli@cisco.com>
2015-09-23 16:27:51 -0700

    arrays

--- helloword1.js
+++ helloword1.js
@@ -1,8 +1,2 @@
-var total = 0;
-var limit = 10;
-var total = 0;
-for (var i = 0; i < limit; i++) {
-	total += i;
-}
-console.log(total)
-	
+var pizzaToppings = ['tomato sauce', 'cheese', 'pepperoni'];
+console.log(pizzaToppings);
