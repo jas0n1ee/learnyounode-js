39d55acb6c2e06caa6668a27358f49ede74763fa
jisli <jisli@cisco.com>
2015-09-23 16:25:27 -0700

    if statement

--- helloword1.js
+++ helloword1.js
@@ -1,3 +1,7 @@
-var n = 128;
-n = n.toString();
-console.log(n);
+var fruit = 'orange';
+if (fruit.length > 5) {
+	console.log("The fruit name has more than five characters.");
+}
+else {
+	console.log("The fruit name has five characters or less.");
+}
