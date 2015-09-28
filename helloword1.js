e5252cc5ac12ba707c3944a4b1eda6f0a1c91d90
jisli <jisli@cisco.com>
2015-09-28 10:08:44 -0700

    scope

--- helloword1.js
+++ helloword1.js
@@ -1,4 +1,15 @@
-function math (a, b, c) {
-	return a + b * c;
-}
-console.log(math(53, 61, 67))
+var a = 1, b = 2, c = 3;
+(function firstFunction(){
+    var b = 5, c = 6;
+    (function secondFunction(){
+        var b = 8;
+		console.log("a: "+a+", b: "+b+",c: "+c);
+        (function thirdFunction(){
+            var a = 7, c = 9;
+            (function fourthFunction(){
+                var a = 1, c = 8;
+            })();
+        })();
+    })();
+})();
+
