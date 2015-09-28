812fb81e9bf2628e9018c218110279411a6f07fb
jisli <jisli@cisco.com>
2015-09-28 09:56:33 -0700

    object properties

--- helloword1.js
+++ helloword1.js
@@ -1,6 +1,4 @@
-var pizza = {
-	toppings: ['cheese', 'sauce', 'pepperoni'],
-	crust: 'deep dish',
-	serves: 2
-}
-console.log(pizza)
+var food = {
+	types: 'only pizza'
+};
+console.log(food['types'])
