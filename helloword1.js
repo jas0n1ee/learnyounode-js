20d348f526b7665d3b7b8314991189ed10ad5dce
jisli <jisli@cisco.com>
2015-09-29 11:56:44 -0700

    time server

--- helloword1.js
+++ helloword1.js
@@ -1,20 +1,16 @@
-var http = require('http')
-var bl = require('bl')
-var list = [];
-var fin = 0;
-function proc(url,i,callback) {
-	http.get(url,function (response) {
-		response.pipe(bl(function (err,data) {
-			if (err) return;
-			list[i] = data.toString();
-			fin ++;
-			callback(list);
-		}));
-	});
+var net = require('net')
+var util = require('util')
+var server = net.createServer(callback)
+server.listen(Number(process.argv[2]));
+// "YYYY-MM-DD hh:mm"
+// "2013-07-06 17:42"
+function callback(socket) {
+	var date = new Date();
+    socket.write(date.getFullYear() + '-' + 
+		 ((date.getMonth()+1)>10 ? (date.getMonth() + 1) : ('0' + (date.getMonth()+1))) + '-' +
+		 (date.getDate()>10 ? date.getDate() : ('0' + date.getDate())) + ' ' +
+		 (date.getHours()>10 ? date.getHours() : ('0' + date.getHours())) + ':' +
+		 (date.getMinutes()>10 ? date.getMinutes() : ('0' + date.getMinutes())) + '\n')
+	socket.end();
 }
-for (var i = 0;i < 3;i++)
-	proc(process.argv[i+2],i,function(list) {
-		if (fin == 3)
-			for(var j = 0; j< 3; j++)
-				console.log(list[j])
-	})
+
