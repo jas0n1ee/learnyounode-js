7a25d104e800f0681c9417c1f37a2ff3615a4ceb
jisli <jisli@cisco.com>
2015-09-29 14:58:11 -0700

    http json api

--- helloword1.js
+++ helloword1.js
@@ -1,12 +1,27 @@
-var http = require('http')
-var fs = require('fs')
-var map = require('through2-map')
-var server = http.createServer(callback)
+var http = require('http');
+var url = require('url');
+var server = http.createServer(callback);
 
 server.listen(Number(process.argv[2]));
 function callback(req,res) {
-	req.pipe(map(function (chunk) {
-		return chunk.toString().toUpperCase();
-	})).pipe(res);
+	res.writeHead(200,{ 'Content-Type': 'application/json' });
+	var parse = url.parse(req.url, true);
+	var date = new Date(parse.query.iso.split('Z')[0]);
+	if (parse.pathname == "/api/parsetime") {
+		var json = JSON.stringify({
+			hour: date.getHours(),
+			minute: date.getMinutes(),
+			second: date.getSeconds()
+		});
+		res.write(json);
+		res.end();
+	}
+	if (parse.pathname == "/api/unixtime") {	
+		var json = JSON.stringify({
+			unixtime : date.getTime()
+		});
+		res.write(json);
+		rew.end();
+	}
 }
 
