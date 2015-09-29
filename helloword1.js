7d77c80ed7300b3aea55c467ae75b71d559a7f41
jisli <jisli@cisco.com>
2015-09-29 11:01:58 -0700

    JUGGLING ASYNC

--- helloword1.js
+++ helloword1.js
@@ -1,10 +1,20 @@
 var http = require('http')
 var bl = require('bl')
-http.get(process.argv[2],function (response) {
-//	response.setEncoding('utf8')
-	response.pipe(bl(function (err,data) {
-		if (err) return;
-		console.log(data.toString().length);
-		console.log(data.toString());
-	}));
-});
+var list = [];
+var fin = 0;
+function proc(url,i,callback) {
+	http.get(url,function (response) {
+		response.pipe(bl(function (err,data) {
+			if (err) return;
+			list[i] = data.toString();
+			fin ++;
+			callback(list);
+		}));
+	});
+}
+for (var i = 0;i < 3;i++)
+	proc(process.argv[i+2],i,function(list) {
+		if (fin == 3)
+			for(var j = 0; j< 3; j++)
+				console.log(list[j])
+	})
