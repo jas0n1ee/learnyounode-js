19ade1429bdb7572c045daca9bd1b3f5bbb01830
jisli <jisli@cisco.com>
2015-09-29 12:39:11 -0700

    http file server

--- helloword1.js
+++ helloword1.js
@@ -1,16 +1,10 @@
-var net = require('net')
-var util = require('util')
-var server = net.createServer(callback)
+var http = require('http')
+var fs = require('fs')
+var server = http.createServer(callback)
+
 server.listen(Number(process.argv[2]));
-// "YYYY-MM-DD hh:mm"
-// "2013-07-06 17:42"
-function callback(socket) {
-	var date = new Date();
-    socket.write(date.getFullYear() + '-' + 
-		 ((date.getMonth()+1)>10 ? (date.getMonth() + 1) : ('0' + (date.getMonth()+1))) + '-' +
-		 (date.getDate()>10 ? date.getDate() : ('0' + date.getDate())) + ' ' +
-		 (date.getHours()>10 ? date.getHours() : ('0' + date.getHours())) + ':' +
-		 (date.getMinutes()>10 ? date.getMinutes() : ('0' + date.getMinutes())) + '\n')
-	socket.end();
+function callback(req,res) {
+	var file = fs.createReadStream(process.argv[3]);
+	file.pipe(res);
 }
 
