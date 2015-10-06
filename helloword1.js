2fcafebebc15edf6235b5f2ecb4b0804abaf0d4b
jisli <jisli@cisco.com>
2015-10-06 13:53:04 -0700

    first try on node-addon

--- helloword1.js
+++ helloword1.js
@@ -1,27 +1,2 @@
-var http = require('http');
-var url = require('url');
-var server = http.createServer(callback);
-
-server.listen(Number(process.argv[2]));
-function callback(req,res) {
-	res.writeHead(200,{ 'Content-Type': 'application/json' });
-	var parse = url.parse(req.url, true);
-	var date = new Date(parse.query.iso.split('Z')[0]);
-	if (parse.pathname == "/api/parsetime") {
-		var json = JSON.stringify({
-			hour: date.getHours(),
-			minute: date.getMinutes(),
-			second: date.getSeconds()
-		});
-		res.write(json);
-		res.end();
-	}
-	if (parse.pathname == "/api/unixtime") {	
-		var json = JSON.stringify({
-			unixtime : date.getTime()
-		});
-		res.write(json);
-		res.end();
-	}
-}
-
+var addon = require('./build/Release/addon');
+console.log(addon.hello());
